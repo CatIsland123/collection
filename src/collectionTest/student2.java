edfce4bd3e741a09dd6b34e73e30569e769727ca
CatIsland123 <1289685016@qq.com>
2019-05-17 20:12:02 +0800

    第二题1

---
+++ src/collectionTest/student2.java
@@ -0,0 +1,28 @@
+package collectionTest;
+
+public class student2 {
+	
+		private String name;
+		int age;
+		student2(String n,int age){
+			this.name=n;
+			this.age=age;
+			
+		}
+		public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public void setAge(int age) {
+	        this.age = age;
+	    } 
+	}
+
