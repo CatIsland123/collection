ec0db43ed4507b53e62fa8ae0595005923b263a3
CatIsland123 <1289685016@qq.com>
2019-05-17 21:37:32 +0800

    第三题2

--- src/collectionTest/MapVote.java
+++ src/collectionTest/MapVote.java
@@ -12,14 +12,22 @@ public class MapVote {
        for(int i=0;i<3;i++) {
     	   map.put(candidate[i], 0);
        }
-       
-       Scanner in=new Scanner(System.in);
-       Integer vote=in.nextInt();
-       if(map.containsKey(vote)) {
-    	   Integer t=map.get(vote);
-    	   map.put(vote, ++t);
+       for(int j=0;j<10;j++) {
+    	   for(int k=0;k<2;k++) { 
+    		    System.out.println(j+":请输入您所选择的候选人号码：");
+                Scanner in=new Scanner(System.in);
+                Integer vote=in.nextInt();
+                if(vote>3||vote<0) {
+                	System.out.println(j+":请重新输入您所选择的候选人号码：");
+                    Scanner in1=new Scanner(System.in);
+                    Integer vote1=in1.nextInt();
+                }
+                if(map.containsKey(vote)) {
+    	        Integer t=map.get(vote);
+    	        map.put(vote, ++t);
        }
-       
+       }
+       }
        map.forEach((key, value) -> System.out.println(key + "    :    " + value));
        
 	}
