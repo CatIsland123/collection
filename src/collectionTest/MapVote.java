3512d5a44edd0c6ffd2877be71f28e45e5f8b9bc
CatIsland123 <1289685016@qq.com>
2019-05-17 21:45:55 +0800

    第三题3

--- src/collectionTest/MapVote.java
+++ src/collectionTest/MapVote.java
@@ -7,14 +7,15 @@ public class MapVote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
        int []candidate= {1,2,3};
        HashMap<Integer,Integer>map=new HashMap<Integer,Integer>();
        for(int i=0;i<3;i++) {
     	   map.put(candidate[i], 0);
        }
-       for(int j=0;j<10;j++) {
+       for(int j=1;j<=10;j++) {
+    	   System.out.println(j+":请输入您所选择的候选人号码：");
     	   for(int k=0;k<2;k++) { 
-    		    System.out.println(j+":请输入您所选择的候选人号码：");
                 Scanner in=new Scanner(System.in);
                 Integer vote=in.nextInt();
                 if(vote>3||vote<0) {
@@ -29,7 +30,8 @@ public class MapVote {
        }
        }
        map.forEach((key, value) -> System.out.println(key + "    :    " + value));
-       
+       //Map遍历Map集合的方法
+		
 	}
 
 }
