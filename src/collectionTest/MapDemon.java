55c42bf1b58d8f24418d5e8a2bceddf51eaf59ef
CatIsland123 <1289685016@qq.com>
2019-05-17 20:54:09 +0800

    第二题2

--- src/collectionTest/MapDemon.java
+++ src/collectionTest/MapDemon.java
@@ -1,10 +1,35 @@
 package collectionTest;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import collectionTest.student2;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class MapDemon {
-
+  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+         Map<student2,String>map=new HashMap<student2,String>();
+         String addr="福建福州市";
+         map.put(new student2("Liua",20), addr);
+         map.put(new student2("Nic",21), addr);
+         map.put(new student2("Jane",21), addr);
+         //使用keySet
+          Set<student2> keyset=map.keySet();
+          Iterator it=keyset.iterator();
+          while(it.hasNext()) {
+        	  System.out.println(it.next()+"地址："+map.get(it.next()));
+          }
+          //使用entry
+          Iterator it1=map.entrySet().iterator();
+          while(it1.hasNext()) {
+		  Map.Entry<student2, String>entry=(Entry<student2, String>) it1.next();
+          System.out.println(entry.getKey()+" "+entry.getValue());
+          }
 	}
+
+	
 
 }
--- src/collectionTest/student2.java
+++ src/collectionTest/student2.java
@@ -23,6 +23,28 @@ public class student2 {
 
 	    public void setAge(int age) {
 	        this.age = age;
-	    } 
+	    }
+		@Override
+		public String toString() {
+			return "student2 [name=" + name + ", age=" + age + "]";
+		}
+		@Override
+		public int hashCode() {
+			int result ;
+			result = (this.name == null) ? 0 : name.hashCode();
+			result+=this.age;
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if(!(obj instanceof student2))
+				return false;
+			student2 s=(student2)obj;
+			if((this.name.equals(s.name))&&(this.age==s.age))
+				return true;
+			else
+				return false;
+			
+		} 
 	}
 
