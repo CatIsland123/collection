fad3fa01aa69760eaf2e0ac3cb1752d53a9a230a
CatIsland123 <1289685016@qq.com>
2019-05-18 13:31:51 +0800

    hashsetc第二次修改

--- src/collectionTest/HashSetDemon.java
+++ src/collectionTest/HashSetDemon.java
@@ -17,6 +17,26 @@ class student3{
 	public String toString() {
 		return "student3 [name=" + name + ", age=" + age + "]";
 	}
+	@Override
+	public int hashCode() {
+		System.out.println(this.name+"调用hashCode");
+		int result;
+		result = (this.name==null)?0:name.hashCode();
+		result+=age; 
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println(this.name+"调用equals");
+		if(!(obj instanceof student3))
+			return false;
+		student3 s=(student3) obj;
+		if(this.name.equals(s.name)&&this.age==s.age)
+			return true;
+		else
+			return false;
+		
+	}
 	public int getAge() {
 		  return this.age;
 	  }
@@ -29,6 +49,10 @@ public class HashSetDemon {
 		HashSet<student3> hs=new HashSet<student3>();
 	     hs.add(new student3("LIu",20));
 	     hs.add(new student3("Nick",21));
+	     hs.add(new student3("Aick",21));
+	     hs.add(new student3("Nick",21));
+	     hs.add(new student3("LIu",20));
+	     hs.add(new student3("LIu",25));
 	     Iterator it=hs.iterator();
 	     while(it.hasNext())
 	    	 System.out.println(it.next());
